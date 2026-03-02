e965f17b9ce692bdc4e89b15b41aefd5f55365a1
tiendatmagic <tiendatmagic8@gmail.com>
2026-03-02 12:38:35 +0700

    update env

--- src/app/app-env.ts
+++ src/app/app-env.ts
@@ -1,7 +1,7 @@
 export const appEnv = {
-  isProduction: false,
+  isProduction: true,
   apiBaseDev: 'http://127.0.0.1:8000',
-  apiBaseProduction: 'https://apidev.miummo.com',
+  apiBaseProduction: 'https://apidev.miummo.com/taptoearn',
   telegramBotUrl: 'https://t.me/ttotaptoearnbot',
 } as const;
 
